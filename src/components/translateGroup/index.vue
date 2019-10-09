c45380a0be0015ec07a59ad8c1ce4ff6550f4a0d
SmilePig <306826385@qq.com>
2019-10-09 20:57:20 +0800

    tables

--- src/components/translateGroup/index.vue
+++ src/components/translateGroup/index.vue
@@ -1,30 +1,30 @@
 <template>
   <ul>
-    <translateItem 
-    v-for="(item,index) in data" 
-    :key="index"
-    :data="item"
-    ></translateItem>
+    <translateItem
+      v-for="(item,index) in data"
+      :key="index"
+      :data="item"
+    />
   </ul>
 </template>
 
 <script>
-import translateItem from "../translateItem/index.vue";
+import translateItem from '../translateItem/index.vue'
 
 const FORM_TYPES = {
-  input: "input",
-  range: "range",
-  select: "select",
-  multiSelect: "multiSelect",
-  booleanSelect: "booleanSelect",
-  rangeDate: "rangeDate",
-  singleDate: "singleDate"
-};
+  input: 'input',
+  range: 'range',
+  select: 'select',
+  multiSelect: 'multiSelect',
+  booleanSelect: 'booleanSelect',
+  rangeDate: 'rangeDate',
+  singleDate: 'singleDate'
+}
 
 const BOOLEAN_SELECT_OPTIONS = [
-  { value: "true", label: "是" },
-  { value: "false", label: "否" }
-];
+  { value: 'true', label: '是' },
+  { value: 'false', label: '否' }
+]
 
 // const FETCH_OPTIONS_FNS = {
 //   sellerAlphaId: getOrangeSellerList,
@@ -36,292 +36,292 @@ const BOOLEAN_SELECT_OPTIONS = [
 //   intentLevel: fetchIntentLevel
 // }
 export default {
+  components: {
+    translateItem
+  },
   data() {
     return {
       data: {
         name: {
           type: FORM_TYPES.input,
-          label: "姓名",
-          key: "name",
+          label: '姓名',
+          key: 'name',
           default: null,
-          placeholder: "输入姓名"
+          placeholder: '输入姓名'
         },
         institution: {
           type: FORM_TYPES.input,
-          label: "所在机构",
-          key: "institution",
+          label: '所在机构',
+          key: 'institution',
           default: null,
-          placeholder: "请填写所在机构"
+          placeholder: '请填写所在机构'
         },
         phone: {
           type: FORM_TYPES.input,
-          label: "手机号",
-          key: "phone",
+          label: '手机号',
+          key: 'phone',
           default: null,
-          placeholder: "请填写手机号"
+          placeholder: '请填写手机号'
         },
         position: {
           type: FORM_TYPES.multiSelect,
-          label: "职务",
-          key: "position",
-          placeholder: "请选择职务",
+          label: '职务',
+          key: 'position',
+          placeholder: '请选择职务',
           default: null,
-          configskey: "position"
+          configskey: 'position'
         },
         courseNumber: {
           type: FORM_TYPES.range,
-          label: "课程次数",
-          minKey: "minCourseNumber",
-          maxKey: "maxCourseNumber",
+          label: '课程次数',
+          minKey: 'minCourseNumber',
+          maxKey: 'maxCourseNumber',
           default: null
         },
         isAlphaUser: {
           type: FORM_TYPES.booleanSelect,
-          label: "是否Alpha用户",
-          key: "isAlphaUser",
-          placeholder: "是否Alpha用户",
+          label: '是否Alpha用户',
+          key: 'isAlphaUser',
+          placeholder: '是否Alpha用户',
           default: null,
           options: BOOLEAN_SELECT_OPTIONS
         },
         isIcourtStudent: {
           type: FORM_TYPES.booleanSelect,
-          label: "是否iCourt学员",
-          key: "isIcourtStudent",
-          placeholder: "是否iCourt学员",
+          label: '是否iCourt学员',
+          key: 'isIcourtStudent',
+          placeholder: '是否iCourt学员',
           default: null,
           options: BOOLEAN_SELECT_OPTIONS
         },
         coursesTakenId: {
           type: FORM_TYPES.multiSelect,
-          label: "上过的课程",
-          key: "coursesTakenId",
+          label: '上过的课程',
+          key: 'coursesTakenId',
           default: null,
           filterable: true
           // fetchOptionFn: FETCH_OPTIONS_FNS.coursesTakenId
         },
         courseName: {
           type: FORM_TYPES.input,
-          label: "课程名",
-          key: "courseName",
+          label: '课程名',
+          key: 'courseName',
           default: null
         },
         province: {
           type: FORM_TYPES.multiSelect,
-          label: "选择省",
-          key: "province",
-          placeholder: "选择省",
+          label: '选择省',
+          key: 'province',
+          placeholder: '选择省',
           default: null
         },
         city: {
           type: FORM_TYPES.multiSelect,
-          label: "选择市",
-          key: "city",
-          placeholder: "选择市",
+          label: '选择市',
+          key: 'city',
+          placeholder: '选择市',
           default: null
         },
         area: {
           type: FORM_TYPES.multiSelect,
-          label: "选择区县",
-          key: "area",
-          placeholder: "选择区县",
+          label: '选择区县',
+          key: 'area',
+          placeholder: '选择区县',
           default: null
         },
         tags: {
           type: FORM_TYPES.multiSelect,
-          label: "标签",
-          key: "tags",
-          placeholder: "选择标签",
+          label: '标签',
+          key: 'tags',
+          placeholder: '选择标签',
           default: null,
-          configskey: "userTags"
+          configskey: 'userTags'
         },
         userTagsFlag: {
           type: FORM_TYPES.booleanSelect,
-          label: "是否为排除标签中所选",
-          key: "userTagsFlag",
-          placeholder: "是否为排除标签中所选",
+          label: '是否为排除标签中所选',
+          key: 'userTagsFlag',
+          placeholder: '是否为排除标签中所选',
           default: null,
           options: BOOLEAN_SELECT_OPTIONS
         },
         inviteStatus: {
           type: FORM_TYPES.multiSelect,
-          label: "邀请状态",
-          key: "stockrightInviteStatus",
-          placeholder: "选择邀请状态",
+          label: '邀请状态',
+          key: 'stockrightInviteStatus',
+          placeholder: '选择邀请状态',
           default: null,
-          configskey: "stockrightInviteStatus",
-          appendOptions: [{ label: "空", value: "空" }]
+          configskey: 'stockrightInviteStatus',
+          appendOptions: [{ label: '空', value: '空' }]
         },
         inviter: {
           type: FORM_TYPES.multiSelect,
-          label: "邀请人",
-          key: "inviter",
-          placeholder: "选择邀请人",
+          label: '邀请人',
+          key: 'inviter',
+          placeholder: '选择邀请人',
           default: null,
           filterable: true,
           // fetchOptionFn: FETCH_OPTIONS_FNS.sellerAlphaId,
-          appendOptions: [{ label: "空", value: "空" }]
+          appendOptions: [{ label: '空', value: '空' }]
         },
         myInvitionTime: {
           type: FORM_TYPES.rangeDate,
-          label: "邀请时间筛选",
-          minKey: "minInvitationTime",
-          maxKey: "maxInvitationTime",
+          label: '邀请时间筛选',
+          minKey: 'minInvitationTime',
+          maxKey: 'maxInvitationTime',
           default: null
         },
         content: {
           type: FORM_TYPES.input,
-          label: "跟进记录",
-          key: "content",
+          label: '跟进记录',
+          key: 'content',
           default: null,
-          placeholder: "跟进记录"
+          placeholder: '跟进记录'
         },
         lastContactTime: {
           type: FORM_TYPES.singleDate,
-          label: "最后联系时间",
-          key: "lastContactTime",
+          label: '最后联系时间',
+          key: 'lastContactTime',
           default: null
         },
         intentLevel: {
           type: FORM_TYPES.multiSelect,
-          label: "邀请级别",
-          key: "intentLevel",
+          label: '邀请级别',
+          key: 'intentLevel',
           default: null,
-          configskey: "intentLevel"
+          configskey: 'intentLevel'
           // fetchOptionFn: FETCH_OPTIONS_FNS.intentLevel
         },
         orangePhone: {
           type: FORM_TYPES.input,
-          label: "橙子手机",
-          key: "orangePhone",
+          label: '橙子手机',
+          key: 'orangePhone',
           default: null
         },
         chatGroup: {
           type: FORM_TYPES.input,
-          label: "所在群",
-          key: "chatGroup",
+          label: '所在群',
+          key: 'chatGroup',
           default: null
         },
         remark: {
           type: FORM_TYPES.input,
-          label: "备注",
-          key: "remark",
+          label: '备注',
+          key: 'remark',
           default: null
         },
         lengthOfPractice: {
           type: FORM_TYPES.range,
-          label: "执业年限",
-          minKey: "minLengthOfPractice",
-          maxKey: "maxLengthOfPractice",
-          placeholder: "请输入执业年限",
+          label: '执业年限',
+          minKey: 'minLengthOfPractice',
+          maxKey: 'maxLengthOfPractice',
+          placeholder: '请输入执业年限',
           default: null
         },
         birthday: {
           type: FORM_TYPES.rangeDate,
-          label: "出生日期",
-          minKey: "minBirthday",
-          maxKey: "maxBirthday",
-          placeholder: "请输入出生日期",
+          label: '出生日期',
+          minKey: 'minBirthday',
+          maxKey: 'maxBirthday',
+          placeholder: '请输入出生日期',
           default: null
         },
         futureLawFlag: {
           type: FORM_TYPES.booleanSelect,
-          label: "是否参加活动",
-          key: "futureLawFlag",
+          label: '是否参加活动',
+          key: 'futureLawFlag',
           default: null,
           options: BOOLEAN_SELECT_OPTIONS
         },
         activityType: {
           type: FORM_TYPES.multiSelect,
-          label: "活动类型",
-          key: "activityType",
-          placeholder: "活动类型",
+          label: '活动类型',
+          key: 'activityType',
+          placeholder: '活动类型',
           default: null,
           filterable: true,
-          configskey: "activitySysType"
+          configskey: 'activitySysType'
         },
         sign: {
           type: FORM_TYPES.booleanSelect,
-          label: "是否签到",
-          key: "sign",
+          label: '是否签到',
+          key: 'sign',
           default: null,
           options: BOOLEAN_SELECT_OPTIONS
         },
         joinActivityId: {
           type: FORM_TYPES.multiSelect,
-          label: "参加时间",
-          key: "joinActivityId",
-          placeholder: "参加时间",
+          label: '参加时间',
+          key: 'joinActivityId',
+          placeholder: '参加时间',
           default: null
           // fetchOptionFn: FETCH_OPTIONS_FNS.joinActivityId
         },
         isOrNot: {
           type: FORM_TYPES.booleanSelect,
-          label: "是否购买该产品",
-          key: "isOrNot",
+          label: '是否购买该产品',
+          key: 'isOrNot',
           default: null,
           options: BOOLEAN_SELECT_OPTIONS
         },
         type: {
           type: FORM_TYPES.multiSelect,
-          label: "产品类型",
-          key: "type",
-          placeholder: "产品类型",
+          label: '产品类型',
+          key: 'type',
+          placeholder: '产品类型',
           default: null,
           filterable: true,
-          configskey: "customerIntent"
+          configskey: 'customerIntent'
         },
         activityId: {
           type: FORM_TYPES.multiSelect,
-          label: "产品名称",
-          key: "activityId",
-          placeholder: "产品名称",
+          label: '产品名称',
+          key: 'activityId',
+          placeholder: '产品名称',
           default: null,
           filterable: true
           // fetchOptionFn: FETCH_OPTIONS_FNS.activityId
         },
         lastLoginTime: {
           type: FORM_TYPES.rangeDate,
-          label: "最近一次登录时间",
-          minKey: "minLastLoginTime",
-          maxKey: "maxLastLoginTime",
+          label: '最近一次登录时间',
+          minKey: 'minLastLoginTime',
+          maxKey: 'maxLastLoginTime',
           default: null
         },
         contactSellerId: {
           type: FORM_TYPES.multiSelect,
-          label: "跟进人",
-          key: "contactSellerId",
+          label: '跟进人',
+          key: 'contactSellerId',
           default: null,
           filterable: true
           // fetchOptionFn: FETCH_OPTIONS_FNS.contactSellerId
         },
         inviterTime: {
           type: FORM_TYPES.rangeDate,
-          label: "计划跟进日期",
-          minKey: "feedDateStart",
-          maxKey: "feedDateEnd",
+          label: '计划跟进日期',
+          minKey: 'feedDateStart',
+          maxKey: 'feedDateEnd',
           default: null
         },
         monthReqTimes: {
           type: FORM_TYPES.select,
-          label: "近一个月使用次数",
-          key: "monthReqTimes",
+          label: '近一个月使用次数',
+          key: 'monthReqTimes',
           default: null,
-          configskey: "alphaMonthReqTimes"
+          configskey: 'alphaMonthReqTimes'
         },
         upDataTime: {
           type: FORM_TYPES.rangeDate,
-          label: "新增邀请日期",
-          minKey: "increaseInviterBeginTime",
-          maxKey: "increaseInviterEndTime",
+          label: '新增邀请日期',
+          minKey: 'increaseInviterBeginTime',
+          maxKey: 'increaseInviterEndTime',
           default: null
         }
       }
-    };
-  },
-  components: {
-    translateItem
+    }
   }
-};
+}
 </script>
 
 <style lang="scss">
--- src/views/enterprise/index.vue
+++ src/views/enterprise/index.vue
@@ -1,19 +1,294 @@
 <template>
   <div>
-    <translateGroup></translateGroup>
-    this is Enterprise page
+    <!-- <translateGroup /> -->
+    <div style="position: relative;">
+      <i-table
+        :columns="columns"
+        :data="data"
+      />
+      <div
+        :style="{'background-image': 'url('+ watermarkbgUrl +')'}"
+        class="water-mark"
+      />
+      <Page
+        :total="100"
+        show-elevator
+        show-sizer
+        show-total
+        class="ban-trigger"
+      />
+    </div>
   </div>
 </template>
 
 <script>
-import translateGroup from '@/components/translateGroup/index.vue'
+// import translateGroup from '@/components/translateGroup/index.vue'
 export default {
-  components:{
-    translateGroup
+  components: {
+    // translateGroup
+  },
+  data() {
+    return {
+      // * 列表格式
+      columns: [
+        { type: 'selection', fixed: 'left', width: 40 },
+        {
+          title: '姓名',
+          key: 'name',
+          sortable: 'custom',
+          fixed: 'left',
+          width: 100,
+          render: (h, params) => {
+            return h('span', {
+              on: {
+                click: () => {
+                  window.open(`#/client-overview?id=${params.row.baseId}&from=invitelist`, '_blank')
+                }
+              },
+              style: {
+                cursor: 'pointer'
+              }
+            }, params.row.name)
+          }
+        },
+        { title: '所在机构', key: 'institution', width: 200 },
+        { title: '是否Alpha律所', key: 'isAlphaIns', width: 100, render: (h, params) => h('span', params.row.isAlphaIns ? '是' : '否') },
+        { title: '职务', key: 'position', width: 100 },
+        { title: '执业年限', key: 'lengthOfPractice', width: 100, sortable: 'custom' },
+        {
+          title: '手机号',
+          key: 'mobilePhone',
+          width: 120,
+          render: (h, params) => {
+            if (params.row.partnerOwnStatus === 1) {
+              return h('div', params.row.mobilePhone)
+            } else if (params.row.mobilePhone) {
+              return h('div', '***********')
+            }
+          }
+        },
+        {
+          title: '最后更新时间',
+          key: 'gmtModify',
+          sortable: 'custom',
+          minWidth: 150
+          //   render: (h, params) => {
+          //     return h('span', params.row.gmtModify ? Util.fmtDate(params.row.gmtModify, 'yyyy-MM-dd hh:mm:ss') : '')
+          //   }
+        },
+        {
+          title: '区域',
+          width: 140,
+          render: (h, params) => {
+            return h('span', `${params.row.province || ''} ${params.row.city || ''} ${params.row.area || ''}`)
+          }
+        },
+        { title: '邀请状态', key: 'partnerInviteStatus', align: 'center', minWidth: 140 },
+        { title: '邀请人', key: 'partnerInviter', align: 'center', minWidth: 73 },
+        {
+          title: '邀请记录',
+          key: 'partnerInviteRecord',
+          minWidth: 180,
+          render: (h, params) => {
+            return h('p', {
+              style: {
+                maxHeight: '150px',
+                overflow: 'scroll'
+              }
+            }, this.getList(params.row.partnerInviteRecord).map(item => h('div', item)))
+          }
+        },
+        {
+          title: '计划跟进日期',
+          key: 'partnerInviterTime',
+          minWidth: 130,
+          sortable: 'custom'
+          //   render: (h, params) => {
+          //     return h('span', params.row.partnerInviterTime ? Util.fmtDate(params.row.partnerInviterTime, 'yyyy-MM-dd') : '')
+          //   }
+        },
+        {
+          title: 'Alpha用户',
+          key: 'isAlphaUser',
+          width: 100,
+          render: (h, params) => {
+            return h('span', params.row.isAlphaUser ? '是' : '否')
+          }
+        },
+        {
+          title: 'Alpha用户最近登录时间',
+          key: 'lastLoginTime',
+          sortable: 'custom',
+          width: 170
+          //   render: (h, params) => {
+          //     return h('span', params.row.lastLoginTime ? Util.fmtDate(params.row.lastLoginTime, 'yyyy-MM-dd hh:mm:ss') : '')
+          //   }
+        },
+        {
+          title: '近一个月的使用次数',
+          key: 'monthReqTimesStr',
+          sortable: 'custom',
+          width: 150
+        },
+        {
+          title: 'iCourt学员',
+          key: 'isIcourtStudent',
+          width: 100,
+          render: (h, params) => {
+            return h('span', params.row.isIcourtStudent ? '是' : '否')
+          }
+        },
+        {
+          title: '参加课程次数',
+          key: 'courseCount',
+          minWidth: 100
+        },
+        {
+          title: '是否线上法学院',
+          width: 120,
+          sortable: 'custom',
+          render: (h, params) => {
+            return h('span', params.row.isLawCourseOnline ? '是' : '否')
+          }
+        },
+        {
+          title: '美国行学员',
+          key: 'isUsaStudent',
+          width: 80,
+          render: (h, params) => {
+            return h('span', params.row.isUsaStudent ? '是' : '否')
+          }
+        },
+        {
+          title: '是否未来律所',
+          key: 'isFutureLaw',
+          width: 120,
+          render: (h, params) => {
+            return h('span', params.row.isFutureLaw ? '是' : '否')
+          }
+        },
+        { title: '标签', key: 'tags', width: 100 },
+        { title: '邀请级别', key: 'partnerIntentLevel', align: 'center', minWidth: 73 },
+        { title: '决策点', key: 'decisionPoint', align: 'center', minWidth: 73 },
+        { title: '橙子手机', key: 'partnerOrangePhone', align: 'center', minWidth: 73 },
+        { title: 'Alpha用户数', key: 'row', align: 'center', minWidth: 100 },
+        {
+          title: '出生日期',
+          key: 'birthday',
+          width: 120,
+          sortable: 'custom'
+          //   render: (h, params) => h('span', Util.fmtDate(params.row.birthday, 'yyyy-MM-dd'))
+        },
+        {
+          title: '最后联系时间',
+          key: 'lastContactTime',
+          minWidth: 160,
+          align: 'center',
+          render: (h, params) => {
+            return h('span', params.row.lastContactTime)
+          }
+        },
+        {
+          title: '参加时间',
+          key: 'partnerJoinActivityId',
+          minWidth: 160,
+          align: 'center'
+        },
+        { title: '参加人数', key: 'partnerJoinActivityPersonNum', align: 'center', minWidth: 70 },
+        { title: '所在群', key: 'partnerChatGroup', align: 'center', minWidth: 70 },
+        { title: '备注', key: 'partnerRemark', align: 'center', minWidth: 70 },
+        {
+          title: '操作',
+          width: 210,
+          fixed: 'right',
+          align: 'center'
+          //   render: (h, params) => {
+          //     return h(inviteButtonGroup, {
+          //       props: {
+          //         index: params.index,
+          //         row: params.row,
+          //         isAllotFlag: this.isAllotFlag
+          //       },
+          //       on: {
+          //         edit: this.edit,
+          //         detail: this.clientDetail,
+          //         myInvite: this.myInvite,
+          //         allot: this.allot
+          //       }
+          //     })
+          //   }
+        }
+      ],
+      data: [
+        {
+          name: '王小明',
+          age: 18,
+          address: '北京市朝阳区芍药居',
+          province: '北京市',
+          city: '朝阳区',
+          zip: 100000
+        },
+        {
+          name: '张小刚',
+          age: 25,
+          address: '北京市海淀区西二旗',
+          province: '北京市',
+          city: '海淀区',
+          zip: 100000
+        },
+        {
+          name: '李小红',
+          age: 30,
+          address: '上海市浦东新区世纪大道',
+          province: '上海市',
+          city: '浦东新区',
+          zip: 100000
+        },
+        {
+          name: '周小伟',
+          age: 26,
+          address: '深圳市南山区深南大道',
+          province: '广东',
+          city: '南山区',
+          zip: 100000
+        },
+        {
+          name: '王小明',
+          age: 18,
+          address: '北京市朝阳区芍药居',
+          province: '北京市',
+          city: '朝阳区',
+          zip: 100000
+        },
+        {
+          name: '张小刚',
+          age: 25,
+          address: '北京市海淀区西二旗',
+          province: '北京市',
+          city: '海淀区',
+          zip: 100000
+        },
+        {
+          name: '李小红',
+          age: 30,
+          address: '上海市浦东新区世纪大道',
+          province: '上海市',
+          city: '浦东新区',
+          zip: 100000
+        },
+        {
+          name: '周小伟',
+          age: 26,
+          address: '深圳市南山区深南大道',
+          province: '广东',
+          city: '南山区',
+          zip: 100000
+        }
+      ]
+    }
   }
 }
 </script>
 
 <style lang="scss">
-  
 </style>
